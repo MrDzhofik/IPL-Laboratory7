f265d1491676078e15e972b6c74798a7caace863
MrDzhofik <djafarovemil04@mail.ru>
2022-11-08 21:29:17 +0300

    First commit

---
+++ Part_1/file.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+# Create and write
+class Files
+  def initialize(arr)
+    @name = 'numbers.txt'
+    @second = 'modified.txt'
+    @file = File.new(@name, 'w')
+    @file.syswrite(arr)
+  end
+
+  def read_first
+    @file = File.new(@name, 'r')
+    @g = @file.read[1...-1].split(',')
+    @file.close
+  end
+
+  def filter
+    @g.map!(&:to_i)
+    @g.select! { |elem| (elem % 3).zero? & (elem % 7).nonzero? }
+  end
+
+  def write_second
+    @file2 = File.new(@second, 'w')
+    @file2.syswrite(@g)
+    @file2.close
+    @file2 = File.new(@second, 'r')
+    @en = @file2.read[1...-1].split(',').map!(&:to_i)
+    @file2.close
+  end
+
+  def get
+    @en
+  end
+end
+
+def create_random_array(len)
+  Array.new(len) { rand(0...1000) }
+end
---
+++ Part_1/get_input.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative 'file'
+
+# input
+# input = gets.split
+# input.map!(&:to_i)
+# f = Files.new(input)
+# f.read_first
+# f.filter
+# f.write_second
+# puts f.get
+
+p Array.new(15) { rand(0...1000) }
