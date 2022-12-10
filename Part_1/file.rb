98b2b837e3c11da929510478f22701c07169b399
MrDzhofik <djafarovemil04@mail.ru>
2022-12-10 14:39:02 +0300

    Changes with Reek

--- Part_1/file.rb
+++ Part_1/file.rb
@@ -5,33 +5,32 @@ class Files
   def initialize(arr)
     @name = '/home/mrdzhofik/User/Dzhafarov/3 semester/IPL/Lab work/IPL-Laboratory7/Part_1/numbers.txt'
     @second = '/home/mrdzhofik/User/Dzhafarov/3 semester/IPL/Lab work/IPL-Laboratory7/Part_1/modified.txt'
-    @file = File.new(@name, 'w')
-    @file.syswrite(arr)
+    file = File.new(@name, 'w')
+    file.syswrite(arr)
   end
 
   def read_first
-    @file = File.new(@name, 'r')
-    @g = @file.read[1...-1].split(',')
-    @file.close
+    file = File.new(@name, 'r')
+    @gil = file.read[1...-1].split(',')
+    file.close
   end
 
   def filter
-    @g.map!(&:to_i)
-    @g.select! { |elem| (elem % 3).zero? & (elem % 7).nonzero? }
+    @gil.map!(&:to_i)
+    @gil.select! { |elem| (elem % 3).zero? & (elem % 7).nonzero? }
   end
 
   def write_second
-    @file2 = File.new(@second, 'w')
-    @file2.syswrite(@g)
-    @file2.close
-    @file2 = File.new(@second, 'r')
-    @en = @file2.read[1...-1].split(',').map!(&:to_i)
-    @file2.close
+    file_sec = File.new(@second, 'w')
+    file_sec.syswrite(@gil)
+    file_sec.close
+    file_sec = File.new(@second, 'r')
+    @sec = file_sec.read[1...-1].split(',').map!(&:to_i)
   end
 
   def get
     p 'Second file:'
-    @en
+    @sec
   end
 end
 
